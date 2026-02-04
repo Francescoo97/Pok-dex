1d12b67f41aae0f11f66ca019fc3923d7addf0b1
Francesco <prova@email.com>
2026-02-04 19:13:48 +0100

    Aggiornati CSS e JS: card espandibili con statistiche e riorganizzazione funzioni

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let pokemon;
 
-// Dati JSON dei Pokèmon e genera card iniziali
+// Fetch dati JSON dei Pokèmon e genera card le card
 fetch("./pokemon.json-master/pokedex.json")
   .then((response) => response.json())
   .then((data) => {
@@ -8,30 +8,7 @@ fetch("./pokemon.json-master/pokedex.json")
     console.log(pokemon);
     generaCards(pokemon);
   })
-  .catch((err) => console.log("errore", err));
-
-function generaCards(listaPokemon) {
-  const pokedex = document.getElementById("pokedex");
-  while (pokedex.firstChild) {
-    pokedex.removeChild(pokedex.firstChild);
-  }
-
-  // Per ogni Pokèmon crea una card
-  listaPokemon.forEach((pokemon) => {
-    const card = `<div class="card">
-        <img src="pokemon.json-master/images/${formatID(pokemon.id)}.png">
-        <h3>${pokemon.name.english}</h3>
-        </div>`;
-    pokedex.insertAdjacentHTML("beforeend", card);
-  });
-}
-
-// Formatta l'ID in 3 cifre
-function formatID(id) {
-  if (id.toString().length == 1) return `00${id}`;
-  if (id.toString().length == 2) return `0${id}`;
-  return id;
-}
+  .catch((err) => console.log("errore nel caricamento", err));
 
 // Ricerca dinamica
 const searchBar = document.getElementById("search-bar");
@@ -42,3 +19,71 @@ searchBar.addEventListener("input", (e) => {
   });
   generaCards(pokemonFiltrati);
 });
+
+// Funzione che genera le card
+function generaCards(listaPokemon) {
+  const pokedex = document.getElementById("pokedex");
+  pokedex.innerHTML = "";
+
+  // Per ogni Pokemon della lista genera immagine, nome, tipo e statistiche
+  listaPokemon.forEach((pokemon) => {
+    const tipi = pokemon.type.join(" / ");
+
+    const stats = Object.entries(pokemon.base)
+      .map(
+        ([stat, value]) => `
+        <li>
+          <span>${stat}</span>
+          <span>${value}</span>
+        </li>
+      `,
+      )
+      .join("");
+
+    const card = `
+      <div class="card" tabindex="0">
+        <img src="pokemon.json-master/images/${formatID(pokemon.id)}.png" alt="${pokemon.name.english}">
+        <h3>${pokemon.name.english}</h3>
+
+        <div class="extra">
+          <p class="types">${tipi}</p>
+          <ul class="stats">
+            ${stats}
+          </ul>
+        </div>
+      </div>
+    `;
+
+    pokedex.insertAdjacentHTML("beforeend", card);
+  });
+
+  aggiungiEventiCard();
+}
+
+// Collega le card al click
+function aggiungiEventiCard() {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    card.addEventListener("click", () => toggleCard(card));
+  });
+}
+
+// Mostra o nasconde la card cliccata
+function toggleCard(cardCliccata) {
+  document.querySelectorAll(".card.expanded").forEach((card) => {
+    if (card !== cardCliccata) {
+      card.classList.remove("expanded");
+    }
+  });
+
+  // Apre o chiude la card cliccata
+  cardCliccata.classList.toggle("expanded");
+}
+
+// Formatta l'ID in 3 cifre
+function formatID(id) {
+  if (id.toString().length == 1) return `00${id}`;
+  if (id.toString().length == 2) return `0${id}`;
+  return id;
+}
